e38ede9c5c624a3e53639e3f12d3606effba56c5
Marinos.Sake <protutor13@hotmail.com>
2025-12-02 22:31:56 +0200

    feat: enable JPA auditing in application

--- src/main/java/gr/Bank_System/BankSystemApplication.java
+++ src/main/java/gr/Bank_System/BankSystemApplication.java
@@ -1,8 +1,10 @@
-package Gr.Bank_System;
+package gr.Bank_System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BankSystemApplication {
 
