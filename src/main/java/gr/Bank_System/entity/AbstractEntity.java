b44ccd2eded5ccbbd76353401754ce939d07b49d
Marinos.Sake <protutor13@hotmail.com>
2025-12-02 22:30:34 +0200

    feat: add abstract base entity with auditing and auto-generated systemId

---
+++ src/main/java/gr/Bank_System/entity/AbstractEntity.java
@@ -0,0 +1,46 @@
+package gr.Bank_System.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @Column(name = "system_id", nullable = false, updatable = false, unique = true)
+    private String systemId;
+
+    @PrePersist
+    protected void onCreate() {
+        if (systemId == null) {
+            systemId = UUID.randomUUID().toString();
+        }
+    }
+
+}
