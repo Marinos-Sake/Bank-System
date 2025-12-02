0fa82edf3c4b2c95fe2858821d0c7c0a5ecbef2c
Marinos.Sake <protutor13@hotmail.com>
2025-12-02 22:07:47 +0200

    First commit

---
+++ src/test/java/Gr/Bank_System/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Gr.Bank_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
