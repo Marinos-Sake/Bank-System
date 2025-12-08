5eeb34d62cef5c03e4d581ca538b20d92f93d994
Marinos.Sake <protutor13@hotmail.com>
2025-12-08 20:10:46 +0200

    feat: add Entity User

---
+++ src/main/java/gr/bank_System/entity/User.java
@@ -0,0 +1,48 @@
+package gr.bank_System.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = {"password"})
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class User extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id", nullable = false, unique = true)
+    private Person person;
+
+    @Column(name = "public_id", nullable = false, unique = true,
+    updatable = false)
+    private String publicId;
+
+    @PrePersist
+    private void ensurePublicId() {
+        if ( publicId  == null || publicId.isBlank()) {
+            publicId = UUID.randomUUID().toString(); }
+    }
+
+
+}
